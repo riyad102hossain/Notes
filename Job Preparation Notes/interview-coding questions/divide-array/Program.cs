6114390d922d274acfa5475a4f37630f7b9a7159
Riyad Hossain <rh005323@gmail.com>
2025-02-08 13:53:46 +0800

    add code

--- Job Preparation Notes/interview-coding questions/divide-array/Program.cs
+++ Job Preparation Notes/interview-coding questions/divide-array/Program.cs
@@ -1,10 +1,12 @@
-﻿int[] arr = { 2,4,5,3,9};
+﻿int[] arr = { 2, 4, 5, 3, 9 };
 
-int mid = arr.Length / 2+1;
+int mid = arr.Length / 2 + 1;
 
-int[] firsthalf=arr.Take(mid).ToArray();
-int[] secondhalf=arr.Skip(mid).ToArray();
+int[] firsthalf = arr.Take(mid).ToArray();
+int[] secondhalf = arr.Skip(mid).ToArray();
 
-Console.WriteLine($"First half : {string.Join(",",firsthalf)}");
-Console.WriteLine($"Second Half: {string.Join(",",secondhalf)}");
+Console.WriteLine($"First half : {string.Join(",", firsthalf)}");
+Console.WriteLine($"Second Half: {string.Join(",", secondhalf)}");
+
+
 
